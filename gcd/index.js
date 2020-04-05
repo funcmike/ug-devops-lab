f73959833441171dded70f9641f936b1d07cfc34
Krzysztof Majk <kmajk@atamari.pl>
2020-04-05 10:36:56 +0200

    add simple gcd app

---
+++ gcd/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const client = redis.createClient({
+    host: 'redis-server',
+    port: '6379'
+});
+
+function gcd(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+      return 0;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+
+
+app.post('/', (req, res) => {{
+    console.log("input:", req.body);
+    var x = parseInt(req.body.x);
+    var y = parseInt(req.body.y);
+    if (x > y) {
+        var key = y+"-"+x
+    } else {
+        var key = x+"-"+y;
+    }
+    console.log("key:", key );
+    client.get(key, (err, gcd_result) => {
+        if (err || !gcd_result ) {
+            var result = toString(gcd(x,y));
+            console.log("calculated result:", result);
+            client.set(key, result);
+            res.send(result);    
+        } else {
+            console.log("cached result:", gcd_result);
+            res.send(gcd_result);
+    
+        }
+    });
+    res.end;
+}});
+
+app.listen(8080, () => {
+    console.log ("Listening on port 8080");
+})
