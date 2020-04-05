2b6b4d3e8c94101b1c245ce887668b36d76ff839
Krzysztof Majk <kmajk@atamari.pl>
2020-04-05 10:54:10 +0200

    format js madness

--- gcd/index.js
+++ gcd/index.js
@@ -14,43 +14,45 @@ const client = redis.createClient({
 });
 
 function gcd(x, y) {
-    if ((typeof x !== 'number') || (typeof y !== 'number')) 
-      return 0;
+    if ((typeof x !== 'number') || (typeof y !== 'number'))
+        return 0;
     x = Math.abs(x);
     y = Math.abs(y);
-    while(y) {
-      var t = y;
-      y = x % y;
-      x = t;
+    while (y) {
+        var t = y;
+        y = x % y;
+        x = t;
     }
     return x;
-  }
+}
 
 
-app.post('/', (req, res) => {{
-    console.log("input:", req.body);
-    var x = parseInt(req.body.x);
-    var y = parseInt(req.body.y);
-    var key =  x+"-"+y;
-    if (y > x) {
-        key = y+"-"+x
-    }
-    console.log("key:", key );
-    client.get(key, (err, gcd_result) => {
-        if (err || !gcd_result ) {
-            var result = gcd(x,y);
-            console.log("calculated result:", result);
-            client.set(key, result);
-            res.send(result.toString());    
-        } else {
-            console.log("cached result:", gcd_result);
-            res.send(gcd_result);
-    
+app.post('/', (req, res) => {
+    {
+        console.log("input:", req.body);
+        var x = parseInt(req.body.x);
+        var y = parseInt(req.body.y);
+        var key = x + "-" + y;
+        if (y > x) {
+            key = y + "-" + x
         }
-    });
-    res.end;
-}});
+        console.log("key:", key);
+        client.get(key, (err, gcd_result) => {
+            if (err || !gcd_result) {
+                var result = gcd(x, y);
+                console.log("calculated result:", result);
+                client.set(key, result);
+                res.send(result.toString());
+            } else {
+                console.log("cached result:", gcd_result);
+                res.send(gcd_result);
+
+            }
+        });
+        res.end;
+    }
+});
 
 app.listen(8080, () => {
-    console.log ("Listening on port 8080");
+    console.log("Listening on port 8080");
 })
