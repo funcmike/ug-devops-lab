2d4b4dcafbd1e22abf823cb2003287c605e88f9b
Krzysztof Majk <kmajk@atamari.pl>
2020-04-19 11:52:26 +0200

    add postgres and env support to app

--- gcd/index.js
+++ gcd/index.js
@@ -1,16 +1,38 @@
+const keys = require('./keys')
 const express = require('express');
+const cors = require('cors');
 const redis = require('redis');
 const process = require('process');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+console.log(keys);
+
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    host: keys.pgHost,
+    port: keys.pgPort,
+    database: keys.pgDb,
+    user: keys.pgUser,
+    password: keys.pgPass,
+})
+
+pgClient.on('error', () => console.log('wtf? better use mysql'))
+
+
+pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err));
+
 const client = redis.createClient({
-    host: 'redis-server',
-    port: '6379'
+    host: keys.redisHost,
+    port: keys.redisPort,
 });
 
 function gcd(x, y) {
@@ -26,6 +48,18 @@ function gcd(x, y) {
     return x;
 }
 
+app.get('/history', (req, res) => {
+    {
+        pgClient
+        .query('SELECT number from values')
+        .then(results => {
+            console.log(results.rows) // ['brianc']
+            res.send(results.rows);
+          })
+        .catch(err => console.log(err));
+    }
+
+});
 
 app.post('/', (req, res) => {
     {
@@ -42,11 +76,13 @@ app.post('/', (req, res) => {
                 var result = gcd(x, y);
                 console.log("calculated result:", result);
                 client.set(key, result);
-                res.send(result.toString());
+                pgClient
+                    .query('INSERT INTO values(number) VALUES ($1::int)', [result])
+                    .catch(err => console.log(err));
+                res.send({'result': result});
             } else {
                 console.log("cached result:", gcd_result);
-                res.send(gcd_result);
-
+                res.send({'result': parseInt(gcd_result)});
             }
         });
         res.end;
